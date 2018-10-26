2b94875104c93360723e75fe315589797b9f2647
ynobuhar <ynobuhar@yahoo-corp.jp>
2018-10-26 11:03:21 +0900

    build react application

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path')
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin]
+};
