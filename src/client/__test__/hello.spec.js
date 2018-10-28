76ce1a227aa76bbff932ea9376d751f1415d857d
ynobuhar <ynobuhar@yahoo-corp.jp>
2018-10-28 21:32:31 +0900

    remove dir

--- src/client/__test__/hello.spec.js
+++
@@ -1,9 +0,0 @@
-import { Hello } from '../hello'
-import React from 'react';
-import { shallow } from "enzyme"
-
-test('sample', () => {
-  //const result = shallow(<Hello />).contains(<h1>Hello!</h1>)
-  //expect(result).toBeTruthy();
-  expect(1).toBe(1);
-})
